9436625dd91753f680b826282f78f1ab86d5c989
Paul Chen <oolongmen@gmail.com>
2018-07-11 10:29:16 +0800

    update

--- .config/i3/scripts/polybar.sh
+++ .config/i3/scripts/polybar.sh
@@ -4,4 +4,9 @@ killall -q polybar
 
 while pgrep -x polybar > /dev/null; do sleep 1; done
 
-polybar top &
+polybar laptop &
+polybar center &
+polybar right &
+
+# if xrandr listmonitor HDMI-2
+# polybar external &
